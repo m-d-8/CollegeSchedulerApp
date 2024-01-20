4145389950e9044a1ffa20eccfad2aad06f54993
misry <dmisry.7.21.04@gmail.com>
2024-01-20 15:37:44 -0500

    Created to-do list
    Added to-do to the gallery view, removed email button from gallery view

--- app/src/main/java/com/example/collegeschedulerapp/MainActivity.java
+++ app/src/main/java/com/example/collegeschedulerapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.collegeschedulerapp;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -16,10 +19,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.collegeschedulerapp.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+
+    private ArrayList<String> toDoList;
+    private ArrayAdapter<String> toDoListAdapter;
+    private  ListView toDoListView;
+    private Button addToListButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +40,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
--- app/src/main/java/com/example/collegeschedulerapp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/collegeschedulerapp/ui/gallery/GalleryFragment.java
@@ -4,17 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.collegeschedulerapp.MainActivity;
+import com.example.collegeschedulerapp.R;
 import com.example.collegeschedulerapp.databinding.FragmentGalleryBinding;
+
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
+    private ArrayList<String> toDos;
+    private ArrayAdapter<String> toDoListAdapter;
+    private ListView listView;
+    private Button addButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +36,29 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final EditText editText = binding.editTextText;
+        listView = binding.toDoListView;
+        addButton = binding.addToListButton;
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addIten(v);
+            }
+        });
+
+        toDos = new ArrayList<>();
+        toDoListAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, toDos);
+        listView.setAdapter(toDoListAdapter);
+
         return root;
+    }
+
+    private void addIten(View v ) {
+        EditText input = binding.editTextText;
+        String toDo = input.getText().toString();
+        input.getText().clear();
+        toDoListAdapter.add(toDo);
     }
 
     @Override
